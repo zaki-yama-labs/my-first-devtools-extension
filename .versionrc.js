bb50a4c36c4370fc7c45b4aae0ed7a59178e7532
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2021-02-24 01:07:55 +0900

    zaki-yama/copy-title-and-url-as-markdown

---
+++ .versionrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  bumpFiles: [
+    {
+      filename: "package.json",
+      type: "json"
+    },
+    {
+      filename: "dist/manifest.json",
+      type: "json"
+    },
+    {
+      filename: "package-lock.json",
+      type: "json"
+    }
+  ]
+};
